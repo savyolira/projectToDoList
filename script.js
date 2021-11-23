cfc22fea88e3b2a7ff737a906621ee5a13666aec
savyolira <savyolira@gmail.com>
2021-11-23 18:32:39 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const adicionar = document.getElementById("criar-tarefa");
 const input = document.querySelector('input');
 const lista = document.getElementById("lista-tarefas");
+
 adicionar.addEventListener("click", function(){
     let item = document.createElement("li");
     item.innerText = input.value;
     lista.appendChild(item);
     input.value = '';
+
+    item.addEventListener("click", function(){
+        item.classList.add("selected")
+    })
 })
+
+
