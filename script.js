e0cbd2ee5c437c30f67cf72c5cec2301084d73f2
savyolira <savyolira@gmail.com>
2021-11-25 22:59:47 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const adicionar = document.getElementById("criar-tarefa");
-const input = document.querySelector('input');
+const input = document.querySelector("input");
 const lista = document.getElementById("lista-tarefas");
+const apagaTudo = document.querySelector("#apaga-tudo");
+const itens = document.getElementsByClassName("itensLista");
 
 adicionar.addEventListener("click", function(){
     let item = document.createElement("li");
     item.innerText = input.value;
+    item.classList.add("itensLista");
     lista.appendChild(item);
-    input.value = '';
+    input.value = '';  
 
-    item.addEventListener("click", function(){
-        item.classList.add("selected")
-    })
 })
 
+lista.addEventListener("click", (item)=>{
+    const itensLista = document.querySelectorAll(".itensLista");
+    for(let i = 0; i < itensLista.length; i+=1){
+        itensLista[i].classList.remove("selected");
+    }
+    item.target.classList.add("selected");
+})
 
+lista.addEventListener("dblclick", (item)=>{
+    item.target.classList.add("completed");
+})
+
+apagaTudo.addEventListener("click", function(){
+    lista.parentNode.removeChild(itens);
+})
